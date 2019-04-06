60be90565f11490c9441364b17c334f0d2d615ea
Birusha <49340406+Birusha@users.noreply.github.com>
2019-04-06 17:46:46 +0500

    Update 24 fipi.py

--- 24 fipi.py
+++ 24 fipi.py
@@ -1,13 +1,14 @@
-n = int(input())
-minDigit = n % 10
+n = int(input("Введите число"))
+minDigit = 9
+
 while n > 0:
     digit =  n % 10
     if digit % 2 == 0:
         if digit < minDigit:
             minDigit = digit
     n = n // 10
-if minDigit == 0:
+if minDigit == 9:
     print("NO")
 else:
     print(minDigit)
-    
+
